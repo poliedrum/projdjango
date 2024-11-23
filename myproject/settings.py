cf761e8071e70daefe6924cd3ee3a58eaff02e4a
Lex Freitas <32302556+poliedrum@users.noreply.github.com>
2024-11-23 14:04:01 -0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,9 +74,20 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':  'django.db.backends.postgresql',
+        'NAME':    'mydb',
+        'USER':     config('DB_USER'),
+        'PASSWORD': config('DB_PW'),
+        #"OPTIONS": {
+        #    "service": "my_service",
+        #    "passfile": ".my_pgpass",
+        #},
+        
     }
 }
 
